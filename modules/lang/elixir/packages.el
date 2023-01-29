600ddc80bb3259dec742ae15ac4a2250a6e58bf9
Cullan Springstead <cullan@springstead.me>
2023-01-29 16:28:02 -0600

    update elixir-ts-mode to latest version

--- modules/lang/elixir/packages.el
+++ modules/lang/elixir/packages.el
@@ -3,7 +3,7 @@
 
 ;; +elixir.el
 
-(package! elixir-ts-mode :pin "c9aef25863433f14f569afaa0cb907b269a1e79d")
+(package! elixir-ts-mode :pin "4c823af9be5739eacff203f0e3f331b7d754c340")
 (package! exunit :pin "0715c2dc2dca0b56c61330eda0690f90cca5f98b")
 (package! elixir-format
   :recipe (:host github :repo "elixir-editors/emacs-elixir" :files ("elixir-format.el"))
