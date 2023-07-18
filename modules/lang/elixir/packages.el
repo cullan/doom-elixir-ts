e749b3965a8c53d9b69a62002e7b813910cce8d5
Cullan Springstead <cullan@springstead.me>
2023-07-18 11:41:27 -0500

    update deps

--- modules/lang/elixir/packages.el
+++ modules/lang/elixir/packages.el
@@ -3,8 +3,8 @@
 
 ;; +elixir.el
 
-(package! elixir-ts-mode :pin "55d2a73fc5aeb550fcc82deedd40c91f0416e9e5")
+(package! elixir-ts-mode :pin "6862c3c2bcaf4df48fb105eee6d8a443d19f0c5c")
 (package! exunit :pin "0715c2dc2dca0b56c61330eda0690f90cca5f98b")
 (package! elixir-format
   :recipe (:host github :repo "elixir-editors/emacs-elixir" :files ("elixir-format.el"))
-  :pin "e0d0466d83ec80ddb412bb1473908a21baad1ec3")
+  :pin "00d6580a040a750e019218f9392cf9a4c2dac23a")
