c10b91c6380ca4a318494d6071025f3b742c26f1
Cullan Springstead <cullan@springstead.me>
2023-08-05 18:27:33 -0500

    remove lsp and flycheck config
    I am using eglot and elixir-ls configured like so:
    
    (after! eglot
      (add-to-list 'eglot-server-programs `((elixir-ts-mode heex-ts-mode) . ("elixir-ls")))
      (add-hook! (elixir-ts-mode heex-ts-mode) #'eglot-ensure))

--- modules/lang/elixir/config.el
+++ modules/lang/elixir/config.el
@@ -35,51 +35,10 @@
     :for "for"
     :return "return" :yield "use")
 
-  (when (modulep! +lsp)
-    (add-hook 'elixir-ts-mode-local-vars-hook #'lsp! 'append)
-    (add-hook 'heex-ts-mode #'lsp 'append)
-    (after! lsp-mode
-      (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]_build\\'")
-      (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\deps\\'")
-      (setq lsp-language-id-configuration
-            (append lsp-language-id-configuration
-                    '((elixir-ts-mode . "elixir")
-                      (heex-ts-mode . "elixir"))))))
-
-  ;; run elixir credo after lsp
-  (when (and (modulep! +lsp) (modulep! :checkers syntax))
-    (add-hook! 'elixir-ts-mode-local-vars-hook :append
-      (add-hook! 'flycheck-mode-hook :local
-        (flycheck-add-next-checker 'lsp '(warning . elixir-credo)))))
-
   (after! highlight-numbers
     (puthash 'elixir-ts-mode
              "\\_<-?[[:digit:]]+\\(?:_[[:digit:]]\\{3\\}\\)*\\_>"
              highlight-numbers-modelist)))
-
-
-(when (modulep! :checkers syntax)
-  (after! flycheck
-    (flycheck-define-checker elixir-credo
-      "An Elixir checker for static code analysis using Credo.
-See `http://credo-ci.org/'."
-      :command ("mix" "credo"
-                (option-flag "--strict" flycheck-elixir-credo-strict)
-                "--format" "flycheck"
-                "--read-from-stdin" source-original)
-      :standard-input t
-      :working-directory flycheck-credo--working-directory
-      :enabled flycheck-credo--working-directory
-      :error-patterns
-      ((info line-start
-             (file-name) ":" line (optional ":" column) ": "
-             (or "F" "R" "C")  ": " (message) line-end)
-       (warning line-start
-                (file-name) ":" line (optional ":" column) ": "
-                (or "D" "W")  ": " (message) line-end))
-      :modes elixir-ts-mode)
-
-    (add-to-list 'flycheck-checkers 'elixir-credo t)))
 
 
 (use-package! elixir-format
