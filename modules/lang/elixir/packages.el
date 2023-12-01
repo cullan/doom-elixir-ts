f262fc386166e1f77ed5425b0fe8c9e2e6311964
Cullan Springstead <cullan@springstead.me>
2023-12-01 08:08:52 -0600

    update dependencies

--- modules/lang/elixir/packages.el
+++ modules/lang/elixir/packages.el
@@ -3,8 +3,11 @@
 
 ;; +elixir.el
 
-(package! elixir-ts-mode :pin "6862c3c2bcaf4df48fb105eee6d8a443d19f0c5c")
-(package! exunit :pin "0715c2dc2dca0b56c61330eda0690f90cca5f98b")
+;; https://github.com/wkirschbaum/elixir-ts-mode
+(package! elixir-ts-mode :pin "23fc038fa783aa0e4d03139c39b98789f439378b")
+;; https://github.com/ananthakumaran/exunit.el
+(package! exunit :pin "e008c89e01e5680473278c7e7bab42842e294e4d")
+;; https://github.com/elixir-editors/emacs-elixir
 (package! elixir-format
   :recipe (:host github :repo "elixir-editors/emacs-elixir" :files ("elixir-format.el"))
   :pin "00d6580a040a750e019218f9392cf9a4c2dac23a")
